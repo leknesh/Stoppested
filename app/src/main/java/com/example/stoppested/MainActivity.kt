742d2f9067438f9d8bd9a6dd56d7dd187c1ed06a
Henriette Leknes <henriette.leknes@fhi.no>
2025-02-22 16:46:04 +0100

    Stopp01: Added location permission handling

--- app/src/main/java/com/example/stoppested/MainActivity.kt
+++ app/src/main/java/com/example/stoppested/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.stoppested
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,15 +8,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import com.example.stoppested.ui.screens.StoppestedViewModel
 import com.example.stoppested.ui.screens.StoppestedScreen
 import com.example.stoppested.ui.theme.StoppestedTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import org.koin.androidx.compose.KoinAndroidContext
 import org.koin.androidx.compose.koinViewModel
 
@@ -33,6 +43,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun App(stoppestedViewModel: StoppestedViewModel = koinViewModel<StoppestedViewModel>() ) {
     Scaffold(
@@ -40,6 +51,35 @@ fun App(stoppestedViewModel: StoppestedViewModel = koinViewModel<StoppestedViewM
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
     ) { innerPadding ->
+        val permissionsState = rememberMultiplePermissionsState(
+            permissions = listOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+
+        var showRationale by rememberSaveable { mutableStateOf(false) }
+        val permissionsGranted = rememberSaveable { mutableStateOf(false) }
+
+        LaunchedEffect(permissionsState) {
+            permissionsState.launchMultiplePermissionRequest()
+        }
+
+        permissionsGranted.value = permissionsState.allPermissionsGranted
+        if (permissionsState.shouldShowRationale) {
+            showRationale = true
+        }
+
+        if (showRationale) {
+            PermissionRationaleDialog(
+                onDismiss = { showRationale = false },
+                onConfirm = {
+                    showRationale = false
+                    permissionsState.launchMultiplePermissionRequest()
+                }
+            )
+        }
+
 
         StoppestedScreen(
             modifier = Modifier
@@ -53,19 +93,21 @@ fun App(stoppestedViewModel: StoppestedViewModel = koinViewModel<StoppestedViewM
     }
 }
 
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PermissionRationaleDialog(onDismiss: () -> Unit, onConfirm: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(stringResource(R.string.permission_required)) },
+        text = { Text(stringResource(R.string.location_permission_required_long)) },
+        confirmButton = {
+            Button(onClick = onConfirm) {
+                Text(stringResource(R.string.grant_permission))
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismiss) {
+                Text(stringResource(R.string.cancel))
+            }
+        }
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StoppestedTheme {
-        Greeting("Android")
-    }
 }
